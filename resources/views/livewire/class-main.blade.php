ad05ac66414893839ed0d756f6f3e9510d14e295
Gokerwow <brilliansyah45@gmail.com>
2025-06-17 09:09:13 +0700

    finishing edit detail kelas

--- resources/views/livewire/class-main.blade.php
+++ resources/views/livewire/class-main.blade.php
@@ -26,7 +26,21 @@
                     </span>
                 </div>
             </div>
+            @if ($isTentorInThisClass)
+                <div class="absolute z-10 top-4 right-4">
+                    <a href="{{ route('lecture.edit', ['lecture' => $lecture]) }}"
+                        class="flex items-center px-4 py-2 bg-white/90 hover:bg-white text-gray-800 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 backdrop-blur-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-blue-600" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                        </svg>
+                        Edit Kelas
+                    </a>
+                </div>
+            @endif
         </div>
+
     </div>
 
     <div class="flex flex-col lg:flex-row gap-8">
